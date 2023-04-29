131c02817f9abae68c0136702ee44d796eb43b61
02Naitsirk <iifrostii@outlook.com>
2023-04-29 18:48:17 -0400

    Generalize variable names for  customization

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -1,19 +1,30 @@
 use statrs::function::erf;
 use std::f64::consts::{PI, SQRT_2};
 
-/// Computes the hazard rate of the standard normal distribution.
+/// Evaluates the hazard rate of the probability distribution.
 pub fn hazard(x: f64) -> f64 {
     if x >= 20.0 {
         // Very good approximation for x > 20:
         return x + 1.0 / x - 2.0 * x.powi(-3) + 10.0 * x.powi(-5) - 74.0 * x.powi(-7);
     }
-    standard_normal_pdf(x) / (standard_normal_cdf(-x))
+    pdf(x) / (cdf(-x))
 }
 
-/// Computes the derivative of the hazard rate of the standard normal distribution.
+/// Evaluates the derivative of the hazard rate of the probability distribution.
 pub fn hazard_prime(x: f64) -> f64 {
     hazard(x) * (hazard(x) - x)
 }
+
+/// Evaluates the density function of the probability distribution.
+fn pdf(x: f64) -> f64 {
+    (-0.5 * x * x).exp() / (2.0 * PI).sqrt() // Normal distribution.
+}
+
+/// Evaluates the distribution function of the probability distribution.
+fn cdf(x: f64) -> f64 {
+    0.5 * erf::erfc(-x / SQRT_2) // Normal distribution.
+}
+
 
 /// For a matrix equation Ax=b, this function takes the augmented matrix (A|b) and returns the solution x.
 pub fn gaussian_elimination(matrix: &mut [Vec<f64>]) -> Vec<f64> {
@@ -62,12 +73,4 @@ fn eliminate(matrix: &mut [Vec<f64>], i: usize) {
             }
         }
     }
-}
-
-fn standard_normal_pdf(x: f64) -> f64 {
-    (-0.5 * x * x).exp() / (2.0 * PI).sqrt()
-}
-
-fn standard_normal_cdf(x: f64) -> f64 {
-    0.5 * erf::erfc(-x / SQRT_2)
 }
