fbc8332e4e9c2ea977734dba6a8a5d822314f1ad
02Naitsirk <iifrostii@outlook.com>
2023-06-13 17:26:17 -0400

    Purge players that have never won/lost

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,50 @@ mod math_utils;
 mod player;
 use player::Player;
 
+const MAX_ITERATIONS: u8 = 10;
+const THRESHOLD: f64 = 1e-12;
+
 fn generate_skill_levels() -> Vec<Player> {
-    const MAX_ITERATIONS: u8 = 10;
-    const THRESHOLD: f64 = 1e-12;
     let mut players = player::get_players();
+
+    // Iteratively purge all players that have never won or never lost.
+    loop {
+        let mut indices = Vec::<usize>::new(); // This will be ordered from smallest to largest.
+
+        for (index, player) in players.iter().enumerate() {
+            let (mut total_wins, mut total_losses) = (0., 0.);
+
+            for opponent in players.iter() {
+                let (player_wins, opponent_wins) = player.get_score_against(opponent);
+                total_wins += player_wins;
+                total_losses += opponent_wins;
+            }
+
+            if total_wins == 0. || total_losses == 0. {
+                indices.push(index);
+            }
+        }
+
+        if indices.is_empty() {
+            // No more players to purge; kill the loop.
+            break;
+        }
+
+        for index in indices.iter().rev() {
+            players.remove(*index);
+        }
+    }
+
     let n = players.len();
+
     for _ in 0..MAX_ITERATIONS {
         let mut gradient = vec![0f64; n + 1];
-        // The hessian matrix augmented with the negative of the gradient matrix:
-        let mut augmented_hessian = vec![vec![1f64; n + 2]; n + 1];
+        let mut augmented_hessian = vec![vec![1f64; n + 2]; n + 1]; // The hessian matrix augmented with the negative of the gradient matrix.
+
         for (i, player) in players.iter().enumerate() {
             let mut gradient_sum = 0f64;
             let mut hessian_sum = 0f64;
+
             for (j, opponent) in players.iter().enumerate() {
                 let (player_wins, opponent_wins) = player.get_score_against(opponent);
                 let skill_difference = opponent.skill - player.skill;
@@ -23,20 +55,25 @@ fn generate_skill_levels() -> Vec<Player> {
                     + opponent_wins * math_utils::hazard_prime(-skill_difference);
                 hessian_sum -= augmented_hessian[i][j];
             }
+
             gradient[i] = gradient_sum;
             augmented_hessian[i][i] = hessian_sum;
             augmented_hessian[i][n + 1] = -gradient[i]; // Augment the negative gradient to the hessian.
         }
+
         augmented_hessian[n][n + 1] = 0f64;
         let add_to_skill_levels = math_utils::gaussian_elimination(&mut augmented_hessian);
+
         for i in 0..n {
             players[i].skill += add_to_skill_levels[i];
         }
+
         let magnitude = add_to_skill_levels
             .iter()
             .map(|x| x * x)
             .sum::<f64>()
             .sqrt();
+
         if magnitude < THRESHOLD {
             break;
         }
@@ -47,6 +84,7 @@ fn generate_skill_levels() -> Vec<Player> {
 fn main() {
     let mut players = generate_skill_levels();
     players.sort_by(|a, b| b.skill.partial_cmp(&a.skill).unwrap());
+
     for player in &players {
         println!("{}, {}", player.username, player.skill);
     }
--- src/player.rs
+++ src/player.rs
@@ -16,17 +16,22 @@ impl Player {
     pub fn get_coins_at(&self, mcc_index: usize) -> i32 {
         self.coin_history[mcc_index]
     }
+
     pub fn get_score_against(&self, opponent: &Player) -> (f64, f64) {
-        let mut score = (1f64, 1f64);
+        let mut score = (0f64, 0f64);
+
         if self.username == opponent.username {
             return score;
         }
+
         for i in 0..MCC_COUNT {
             let player_coins = self.get_coins_at(i);
             let opponent_coins = opponent.get_coins_at(i);
+
             if player_coins == -1 || opponent_coins == -1 {
                 continue;
             }
+
             match player_coins.cmp(&opponent_coins) {
                 Ordering::Greater => score.0 += 1f64,
                 Ordering::Less => score.1 += 1f64,
@@ -36,6 +41,7 @@ impl Player {
                 }
             }
         }
+
         score
     }
 }
@@ -44,11 +50,13 @@ pub fn get_players() -> Vec<Player> {
     let mut players = Vec::new();
     let file = File::open("coins.txt").unwrap();
     let reader = BufReader::new(file);
+
     for line in reader.lines().flatten() {
         let info: Vec<&str> = line.split(',').collect();
         let username = String::from(info[0]);
         let mut coin_history = [-1i32; MCC_COUNT];
         let mut has_played = false;
+
         for i in 1..MCC_COUNT + 1 {
             let coins = info[i].parse().unwrap();
             coin_history[i - 1] = coins;
@@ -56,15 +64,19 @@ pub fn get_players() -> Vec<Player> {
                 has_played = true;
             }
         }
+
         if !has_played {
             continue;
         }
+
         let player = Player {
             coin_history,
             skill: 0f64,
             username,
         };
+
         players.push(player);
     }
+
     players
 }
