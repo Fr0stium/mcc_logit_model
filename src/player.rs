a1c77008150d8e1ad9045e57570e6885adf6367d
02Naitsirk <iifrostii@outlook.com>
2023-06-14 18:18:52 -0400

    Refactor

--- src/player.rs
+++ src/player.rs
@@ -18,30 +18,25 @@ impl Player {
     }
 
     pub fn get_score_against(&self, opponent: &Player) -> (f64, f64) {
-        let mut score = (0f64, 0f64);
-
+        let mut score = (0.0, 0.0);
         if self.username == opponent.username {
             return score;
         }
-
         for i in 0..MCC_COUNT {
             let player_coins = self.get_coins_at(i);
             let opponent_coins = opponent.get_coins_at(i);
-
             if player_coins == -1 || opponent_coins == -1 {
                 continue;
             }
-
             match player_coins.cmp(&opponent_coins) {
-                Ordering::Greater => score.0 += 1f64,
-                Ordering::Less => score.1 += 1f64,
+                Ordering::Greater => score.0 += 1.0,
+                Ordering::Less => score.1 += 1.0,
                 Ordering::Equal => {
                     score.0 += 0.5;
                     score.1 += 0.5;
                 }
             }
         }
-
         score
     }
 }
@@ -50,33 +45,27 @@ pub fn get_players() -> Vec<Player> {
     let mut players = Vec::new();
     let file = File::open("coins.txt").unwrap();
     let reader = BufReader::new(file);
-
     for line in reader.lines().flatten() {
         let info: Vec<&str> = line.split(',').collect();
         let username = String::from(info[0]);
-        let mut coin_history = [-1i32; MCC_COUNT];
+        let mut coin_history = [-1; MCC_COUNT];
         let mut has_played = false;
-
-        for i in 1..MCC_COUNT + 1 {
+        for i in 1..=MCC_COUNT {
             let coins = info[i].parse().unwrap();
             coin_history[i - 1] = coins;
             if !has_played && coins > 0 {
                 has_played = true;
             }
         }
-
         if !has_played {
             continue;
         }
-
         let player = Player {
             coin_history,
-            skill: 0f64,
+            skill: 0.0,
             username,
         };
-
         players.push(player);
     }
-
     players
 }
